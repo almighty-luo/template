e2ebe93d20e03991fe630a9bf12ba9a825a3559b
almighty-luo <1121683285@qq.com>
2022-07-09 01:06:20 +0800

    测试模板

---
+++ .deleteFunc.js
@@ -0,0 +1,7 @@
+module.exports = function deleteFunc (selecData) {
+  return [
+      {path: '/', name: '.gitignore'},
+      {path: '/', name: '.template.json'},
+      {path: '/', name: 'package-lock.json'}
+  ]
+}
