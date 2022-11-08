d971fdb53a8f095a73a2989e797146ab76bfd0a0
luojinmin <1121683285@qq.com>
2022-11-08 10:46:55 +0800

    提交处理函数

--- .deleteFunc.js
+++
@@ -1,7 +0,0 @@
-module.exports = function deleteFunc (selecData) {
-  return [
-      {path: '/', name: '.gitignore'},
-      {path: '/', name: '.template.json'},
-      {path: '/', name: 'package-lock.json'}
-  ]
-}
